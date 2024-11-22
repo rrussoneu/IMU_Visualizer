76a4b0c632c85674f44cc58509db1d53a19d2ea5
Raphael Russo <122693743+rrussoneu@users.noreply.github.com>
2024-11-22 10:17:11 -0500

    change motion

--- src/transport/mock_transport.cpp
+++ src/transport/mock_transport.cpp
@@ -45,6 +45,8 @@ namespace imu_viz {
             data.timestamp = timestamp;
 
             double t = timestamp * 1e-6;
+            // Simple Motion
+            /*
             data.acceleration = Vector3d(
                     std::sin(t),
                     std::cos(t),
@@ -52,6 +54,20 @@ namespace imu_viz {
                     );
 
             data.gyroscope = Vector3d(0,0,1);
+             */
+            // Figure 8 motion
+            data.acceleration = Vector3d(
+                    std::sin(2 * t) * 3.0,
+                    std::sin(t) * std::cos(t) * 3.0,
+                    9.81 + std::sin(t * 0.5) * 0.5
+            );
+            data.gyroscope = Vector3d(
+                    std::sin(t * 0.5) * 0.3,
+                    std::cos(t * 0.5) * 0.3,
+                    1.0
+            );
+
+
 
             if (dataCallback) {
                 dataCallback(data);
